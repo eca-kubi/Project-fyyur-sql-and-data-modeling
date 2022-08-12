f251a625e12a6b4e26649dbd1a1a576e0dce7475
Eric Clinton Appiah-Kubi <ecakubi@gmail.com>
2022-08-12 02:22:40 +0000

    Implemented and tested /venues/<int:venue_id>

--- app.py
+++ app.py
@@ -129,86 +129,35 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        "past_shows": [{
-            "artist_id": 4,
-            "artist_name": "Guns N Petals",
-            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 2,
-        "name": "The Dueling Pianos Bar",
-        "genres": ["Classical", "R&B", "Hip-Hop"],
-        "address": "335 Delancey Street",
-        "city": "New York",
-        "state": "NY",
-        "phone": "914-003-1132",
-        "website": "https://www.theduelingpianos.com",
-        "facebook_link": "https://www.facebook.com/theduelingpianos",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "past_shows": [],
-        "upcoming_shows": [],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 3,
-        "name": "Park Square Live Music & Coffee",
-        "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-        "address": "34 Whiskey Moore Ave",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "415-000-1234",
-        "website": "https://www.parksquarelivemusicandcoffee.com",
-        "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-        "past_shows": [{
-            "artist_id": 5,
-            "artist_name": "Matt Quevedo",
-            "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [{
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 1,
-    }
-    data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-    return render_template('pages/show_venue.html', venue=data)
+    # replace with real venue data from the venues table, using venue_id
+    error = False
+    venue = {}
+    try:
+        # shows the artist page with the given artist_id
+        venue = Venue.query.filter_by(id=venue_id).one()
+        venue.genres = to_genres_list(venue.genres)
+
+        past_shows = db.session.query(Show.start_time, Show.artist_id, Show.id, Show.venue_id, Venue.id, Venue.name,
+                                      Venue.image_link) \
+            .filter(Show.venue_id == venue_id, Show.start_time < func.now()) \
+            .join(Venue, Artist)
+
+        upcoming_shows = db.session.query(Show.start_time, Show.artist_id, Show.id, Show.venue_id, Venue.id, Venue.name,
+                                          Venue.image_link) \
+            .filter(Show.venue_id == venue_id, Show.start_time > func.now()) \
+            .join(Venue, Artist)
+        venue.website = venue.website_link
+        venue.past_shows = past_shows.all()
+        venue.upcoming_shows = upcoming_shows.all()
+        venue.upcoming_shows_count = upcoming_shows.count()
+        venue.past_shows_count = past_shows.count()
+    except Exception as err:
+        error = True
+        flash('An error occurred!')
+        abort(500)
+    finally:
+        db.session.close()
+    return render_template('pages/show_venue.html', venue=venue)
 
 
 #  Create Venue
@@ -403,6 +352,7 @@ def edit_venue(venue_id):
     form.website_link.data = venue.website_link
     form.name.data = venue.name
     form.seeking_description.data = venue.seeking_description
+    form.seeking_talent.data = venue.seeking_talent
     form.image_link.data = venue.image_link
     form.address.data = venue.address
     form.genres.data = to_genres_list(venue.genres)
@@ -416,7 +366,7 @@ def edit_venue_submission(venue_id):
     form = VenueForm(request.form)
     if form.validate():
         try:
-            venue = Artist.query.filter_by(id=venue_id).first()
+            venue = Venue.query.filter_by(id=venue_id).first()
             venue.name = form.name.data
             venue.city = form.city.data
             venue.state = form.state.data
@@ -427,6 +377,7 @@ def edit_venue_submission(venue_id):
             venue.website_link = form.website_link.data
             venue.seeking_talent = form.seeking_talent.data
             venue.seeking_description = form.seeking_description.data
+            venue.address = form.address.data
             db.session.commit()
             flash('Venue info edited successfully!')
         except Exception as err:
