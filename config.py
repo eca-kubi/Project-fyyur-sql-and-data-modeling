26199b2a697f856b3820c346aa5970511f8363ab
Eric Clinton Appiah-Kubi <ecakubi@gmail.com>
2022-08-12 03:21:22 +0000

    Implemented and tested /venues

--- config.py
+++ config.py
@@ -6,10 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
+# IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:addmin300@localhost:5432/projectfyyur'
 
 #Disable SQLALCHEMY_TRACK_MODIFICATIONS
